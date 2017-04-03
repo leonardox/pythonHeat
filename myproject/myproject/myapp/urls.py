61794c436b1c98322602054aea5097ff673d6ece
Leonardo <leonardo.cordeiro@virtus.ufcg.edu.br>
2017-04-03 11:12:09 -0300

    Adicionando metodo de listagem

--- myproject/myproject/myapp/urls.py
+++ myproject/myproject/myapp/urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
-from myproject.myapp.views import list, build
+from myproject.myapp.views import list, build, list_stacks
 
 urlpatterns = [
     url(r'^list/$', list, name='list'),
-    url(r'^build/(?P<user>\w+)/$', build, name='build')
+    url(r'^build/(?P<user>\w+)/$', build, name='build'),
+    url(r'^stacks/$', list_stacks, name='stacks'),
 ]
--- myproject/myproject/myapp/views.py
+++ myproject/myproject/myapp/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import requests
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
@@ -10,6 +11,8 @@ from myproject.myapp.forms import DocumentForm
 from keystoneauth1 import loading
 from keystoneauth1 import session
 from heatclient import client
+
+AUTH_TOKEN = ""
 
 
 def list(request):
@@ -25,6 +28,7 @@ def list(request):
     else:
         form = DocumentForm()  # A empty, unbound form
 
+    # _get_token()
     # Load documents for the list page
     documents = {}
     documents_db = Document.objects.all()
@@ -68,5 +72,61 @@ def build(request, user):
     return HttpResponse("building")
 
 
+def list_stacks(request):
+    # Handle file upload
+
+    #
+    #
+    #
+    #
+    # curl  -H 'X-Auth-Token: gAAAAABY4kZZKo_2qOESqoEj6GOG2YpT1jzfkkIh1Cr9QiwriRLyTSF4DLEn8XqNRMpyO-RMZAkANyPjgJsx3mmI-3jCXIYL1VaG61q8J71Pnj3dPRJT1nWOgBBXB1ekMElaNsuR46W49teS1BQci2E393W3jApMnUS4odes9j4qZG9I8hljqOU'
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    _get_token()
+
+    header = {
+        'X-Auth-Token': AUTH_TOKEN,
+    }
+
+    r = requests.get('http://172.29.236.100:8004/v1/default/stacks', headers=header)
+    print r.text
 
 
+    # Render list page with the documents and the form
+    return HttpResponse({"listagem feita"})
+
+
+def _get_token():
+    header = {
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "auth":
+            {"identity":
+                 {"methods":
+                      ["password"],
+                  "password": {
+                      "user": {"domain":
+                                   {"name": "Default"},
+                               "name": "admin",
+                               "password": "f55597e72a9b8eba7e3dc90891e6484349918499a698e2dc3eb26"}}},
+             "scope":
+                 {"project":
+                      {"domain":
+                           {"name": "Default"},
+                       "name": "admin"}
+                  }
+             }
+    }
+    r = requests.post('https://10.11.4.100:5000/v3/auth/tokens', data=payload)
+    if 'X-Subject-Token' in r.headers:
+        AUTH_TOKEN = r.headers['X-Subject-Token']
+        print AUTH_TOKEN
+    else:
+        print "sem token"
